2e138ba387b5124808a061419501783e7594aafd
Laura <laurabeatriserafim@gmail.com>
2020-07-15 08:38:39 -0300

    style(example): fix indentation

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -46,31 +46,33 @@ const Main: React.FC = () => {
 
         <main className="p-4 mt-4">
           <div className="px-12 space-y-12">
-            {listItems.map((item) => {
-              const itemClasses = classNames("relative cursor-pointer rounded-lg border-4 border-white border-solid", {
-                "border-indigo-500": item.selected,
-              });
+            {
+              listItems.map((item) => {
+                const itemClasses = classNames("relative cursor-pointer rounded-lg border-4 border-white border-solid", {
+                  "border-indigo-500": item.selected,
+                });
 
-              return (
-                <div
-                  key={item.id}
-                  className={itemClasses}
-                  onClick={handleClick(item)}
-                >
-                  <AspectRatio ratio={4 / 5}>
-                    <img
-                      src={item.imageUrl}
-                      className="w-full h-full object-cover rounded-lg"
-                      alt="Travel"
-                      aria-label={item.name}
-                    />
-                  </AspectRatio>
-                  <p className="bg-white m-2 rounded px-2 py-1 bg-opacity-90 text-sm font-semibold absolute bottom-0 inset-x-0">
-                    {item.name}
-                  </p>
-                </div>
-              );
-            })}
+                return (
+                  <div
+                    key={item.id}
+                    className={itemClasses}
+                    onClick={handleClick(item)}
+                  >
+                    <AspectRatio ratio={4 / 5}>
+                      <img
+                        src={item.imageUrl}
+                        className="w-full h-full object-cover rounded-lg"
+                        alt="Travel"
+                        aria-label={item.name}
+                      />
+                    </AspectRatio>
+                    <p className="bg-white m-2 rounded px-2 py-1 bg-opacity-90 text-sm font-semibold absolute bottom-0 inset-x-0">
+                      {item.name}
+                    </p>
+                  </div>
+                );
+              })
+            }
           </div>
         </main>
       </div>
