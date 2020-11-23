39dfb32235219f5dbaaba8cd167fb5bc0e1f35f0
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-23 12:54:02 -0300

    refactor: add reducer and initialize items

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,8 @@
 export const DEFAULT_ITEM_IDENTIFIER = "id";
+
+export const INITIAL_PAYLOAD = {
+  initialItems: [],
+  selectedItems: [],
+  initialSelectedItems: [],
+  itemIdentifier: DEFAULT_ITEM_IDENTIFIER,
+};
