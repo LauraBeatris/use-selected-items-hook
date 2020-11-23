6e778ea1c27f5b1e7bf76c234f3f5489836b4bf4
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-23 12:53:12 -0300

    refactor(hook):  add new types according to reducer

--- src/types.ts
+++ src/types.ts
@@ -1,54 +1,23 @@
-export type DefaultItem = Record<any, any>;
+export type SelectedItem<T> = T & {
+  isSelected: boolean;
+};
 
-export type DefaultItemIdentifierKey = any;
-
-export interface Arguments<T = DefaultItem, K = DefaultItemIdentifierKey> {
+export interface HookArguments<T> {
   initialItems: T[];
-  itemIdentifierKey: K;
+  itemIdentifier?: any;
   initialSelectedItems?: T[];
 }
 
-export type Item<T> = T & {
-  isSelected: boolean;
-}
-
-export interface State<T = DefaultItem, K = DefaultItemIdentifierKey> {
-  items: Item<T>[];
-  itemIdentifierKey: K;
-}
-
-export interface HookReturnValues<T> {
-  items: Item<T>[];
-  selectedItems: Item<T>[];
-  toggleAllItems: () => void;
-  toggleSingleItem: (itemIdentifierValue: any) => void;
+export interface Action {
+  type: ActionType;
+  payload?: any;
 }
 
 export enum ActionType {
   INITIALIZE_ITEMS,
-  TOGGLE_ALL_ITEMS,
-  TOGGLE_SINGLE_ITEM,
+  TOGGLE_ITEM
 }
 
-interface ActionPayloads extends Record<ActionType, any> {
-  [ActionType.TOGGLE_SINGLE_ITEM]: {
-    itemIdentifierValue?: any;
-  };
-  [ActionType.INITIALIZE_ITEMS]: {
-    initialSelectedItems: Item<any>[];
-    initialItems: Item<any>[];
-  };
+export interface State<T = any> extends HookArguments<T> {
+  selectedItems: SelectedItem<T>[];
 }
-
-type ActionMap<M extends Record<string, any>> = {
-  [Key in keyof M]: M[Key] extends undefined
-    ? {
-        type: Key;
-      }
-    : {
-        type: Key;
-        payload: M[Key];
-      }
-};
-
-export type Action = ActionMap<ActionPayloads>[keyof ActionMap<ActionPayloads>]
