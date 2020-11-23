aa61d4e1ce16832dcc04050190e2d0bb1442c2a5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-23 13:20:55 -0300

    refactor: extract reducer to a separate file to improve readability

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,56 +1,24 @@
 import { Reducer } from "react";
-import update from "immutability-helper";
 
 import { State, Action, ActionType } from "./types";
 
 const reducer: Reducer<State, Action> = (state, action) => {
+  const { selectedItems = [], initialSelectedItems = [] } = state;
+
   switch (action.type) {
     case ActionType.INITIALIZE_ITEMS: {
-      const {
-        initialSelectedItems = [],
-        initialItems = [],
-      } = action.payload;
-
-      return update(state, {
-        items: {
-          $set: initialItems.map((item) => ({
-            ...item,
-            isSelected: initialSelectedItems.includes(item),
-          })),
-        },
-      });
+      return {
+        ...state,
+        selectedItems: selectedItems.map((item) => ({
+          ...item,
+          isSelected: initialSelectedItems.includes(item),
+        })),
+      };
     }
 
-    case ActionType.TOGGLE_SINGLE_ITEM: {
-      const { items, itemIdentifierKey } = state;
-
-      const { itemIdentifierValue } = action.payload;
-
-      const itemIndex = items.findIndex((itemFound) => (
-        itemFound[itemIdentifierKey] === itemIdentifierValue
-      ));
-
-      const item = items[itemIndex];
-
-      return update(state, {
-        items: {
-          $splice: [[itemIndex, 1, {
-            ...item,
-            isSelected: !item.isSelected,
-          }]],
-        },
-      });
-    }
-
-    case ActionType.TOGGLE_ALL_ITEMS: {
-      return update(state, {
-        items: {
-          $set: state.items.map(item => ({
-            ...item,
-            isSelected: !item.isSelected,
-          })),
-        },
-      });
+    case ActionType.TOGGLE_ITEM: {
+      // Execute logic
+      return state;
     }
 
     default: {
