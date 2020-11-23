8e1255299099bc5854cef78b4f3c7371d3ff0ea5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-23 12:54:02 -0300

    refactor: add reducer and initialize items

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,49 @@
-import { useEffect } from "react";
+import { Reducer, useReducer, useEffect } from "react";
 
-import { HookArguments } from "./types";
-import { DEFAULT_ITEM_IDENTIFIER } from "./constants";
+import {
+  HookArguments,
+  ActionType,
+  State,
+  Action,
+} from "./types";
+import { DEFAULT_ITEM_IDENTIFIER, INITIAL_PAYLOAD } from "./constants";
+
+const reducer: Reducer<State, Action> = (state, action) => {
+  const { selectedItems = [], initialSelectedItems = [] } = state;
+
+  switch (action.type) {
+    case ActionType.INITIALIZE_ITEMS: {
+      return {
+        ...state,
+        selectedItems: selectedItems.map((item) => ({
+          ...item,
+          isSelected: initialSelectedItems.includes(item),
+        })),
+      };
+    }
+
+    case ActionType.TOGGLE_ITEM: {
+      // Execute logic
+      return state;
+    }
+
+    default: {
+      throw new Error("Unknown action type");
+    }
+  }
+};
 
 function useSelectedItems<T extends Record<any, any>>({
   initialItems = [],
   itemIdentifier = DEFAULT_ITEM_IDENTIFIER,
+  initialSelectedItems = [],
 }: HookArguments<T>) {
+  const [payload] = useReducer(reducer, INITIAL_PAYLOAD, (state) => ({
+    ...state,
+    itemIdentifier,
+    initialSelectedItems,
+  }));
+
   useEffect(() => {
     const itemsWithInvalidIdentifers = initialItems.filter(
       (findItem: T) => {
@@ -28,6 +65,8 @@ function useSelectedItems<T extends Record<any, any>>({
     itemIdentifier,
     initialItems,
   ]);
+
+  return payload;
 }
 
 export default useSelectedItems;
