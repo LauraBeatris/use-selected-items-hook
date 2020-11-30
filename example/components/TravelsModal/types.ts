4799402f92220050d6355baec788ca66c6989c22
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-30 15:23:24 -0300

    fix: use item generic for selected travels on modal

--- example/components/TravelsModal/types.ts
+++ example/components/TravelsModal/types.ts
@@ -1,7 +1,9 @@
+import { Item } from "use-selected-items-hook/types";
+
 import { Travel } from "../../shared/types";
 
 export interface TravelsModalProps {
   isModalOpen: boolean;
-  selectedTravels: Travel[];
+  selectedTravels: Item<Travel>[];
   handleCloseModal: () => void;
 }
