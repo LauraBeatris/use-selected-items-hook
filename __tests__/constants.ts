bffba7d39a346ba308ee2d0806d273dd3b8f05b5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-02-20 20:39:16 -0300

    style(tests): rename variable

--- __tests__/constants.ts
+++ __tests__/constants.ts
@@ -1,7 +1,7 @@
-export const initialTestItems = [
+export const INITIAL_TEST_ITEMS = [
   { id: 1, text: "What's up" },
   { id: 2, text: "Hey there" },
   { id: 3, text: "How you doing?" },
 ];
 
-export type TestItem = typeof initialTestItems[number]
+export type TestItem = typeof INITIAL_TEST_ITEMS[number]
--- __tests__/hook/actions.test.ts
+++ __tests__/hook/actions.test.ts
@@ -2,16 +2,16 @@ import { renderHook, act } from "@testing-library/react-hooks";
 
 import useSelectedItems from "../../src/index";
 import { ERROR_MESSAGES } from "../../src/constants";
-import { initialTestItems, TestItem } from "../constants";
+import { INITIAL_TEST_ITEMS, TestItem } from "../constants";
 
 describe("Hook Actions", () => {
   it("should toggle a single item", () => {
     const { result } = renderHook(() => useSelectedItems<TestItem>({
       itemIdentifierKey: "id",
-      initialItems: initialTestItems,
+      initialItems: INITIAL_TEST_ITEMS,
     }));
 
-    const item = initialTestItems[0];
+    const item = INITIAL_TEST_ITEMS[0];
 
     act(() => result.current.toggleSingleItem(item));
 
@@ -25,16 +25,16 @@ describe("Hook Actions", () => {
   it("should toggle all items", () => {
     const { result } = renderHook(() => useSelectedItems<TestItem>({
       itemIdentifierKey: "id",
-      initialItems: initialTestItems,
+      initialItems: INITIAL_TEST_ITEMS,
     }));
 
     act(() => result.current.toggleAllItems());
 
-    expect(result.current.selectedItems).toEqual(initialTestItems);
+    expect(result.current.selectedItems).toEqual(INITIAL_TEST_ITEMS);
 
     act(() => result.current.toggleAllItems());
 
-    expect(result.current.selectedItems).not.toEqual(initialTestItems);
+    expect(result.current.selectedItems).not.toEqual(INITIAL_TEST_ITEMS);
     expect(result.current.selectedItems.length).toBe(0);
   });
 
@@ -43,7 +43,7 @@ describe("Hook Actions", () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
       itemIdentifierKey: "invalid-key",
-      initialItems: initialTestItems,
+      initialItems: INITIAL_TEST_ITEMS,
     }));
 
     expect(result.error).toEqual(Error(ERROR_MESSAGES.INVALID_ITEM_IDENTIFIER));
