20b1d5ec787677937277c9188ab6081dcaeb8c2b
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-02-20 21:45:32 -0300

    chore(build): bump package to 3.1.13

--- example/components/TravelsModal/types.ts
+++ example/components/TravelsModal/types.ts
@@ -1,9 +1,7 @@
-import { ItemWithSelectedState } from "use-selected-items-hook/dist/types";
-
 import { Travel } from "../../shared/types";
 
 export type TravelsModalProps = {
   isModalOpen: boolean;
-  selectedTravels: ItemWithSelectedState<Travel>[];
+  selectedTravels: Travel[];
   handleCloseModal: () => void;
 }
